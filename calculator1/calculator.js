7609042ae0081a9bd91cd7108cc469fc1d33dcbf
garagalapraveen <138872200+garagalapraveen@users.noreply.github.com>
2023-07-09 11:07:06 +0530

    Add files via upload

---
+++ calculator1/calculator.js
@@ -0,0 +1,14 @@
+var clickSound=new Audio()
+clickSound.src="./button.mp3"
+var clickSound1=new Audio()
+clickSound1.src="./button1.wav"
+
+function clickAudio()
+{
+    clickSound.play()
+}
+function clickAudio1()
+{
+    clickSound1.play()
+
+}
